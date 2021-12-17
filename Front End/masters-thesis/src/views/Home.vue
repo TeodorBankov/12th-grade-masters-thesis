da8777ea29408fb7505b25ea9d0e6919426da6a9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-17 03:13:36 +0200

    add frontend template, will start working on frontend

---
+++ Front End/masters-thesis/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <input v-model="uwu" />
+    <HelloWorld :msg="uwu" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return { uwu: "start" };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+img {
+  margin: 0 auto;
+  width: fit-content;
+  display: block;
+}
+</style>
